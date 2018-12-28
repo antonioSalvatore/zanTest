26806621b61e194a8aece9821ea54017a5ff3839
Antonio Salvatore <antonio.salvatore91@gmail.com>
2018-12-28 21:00:47 +0100

    [#develop] Add some dependencies in the pom; create the Hibernate configuration file; create 3 entities: BandEntity, LoginEntity, MusicianEntity

---
+++ LetsJam/src/main/java/com/letsjam/entities/BandEntity.java
@@ -0,0 +1,157 @@
+package com.letsjam.entities;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+import javax.persistence.*;
+
+@Entity(name ="BandEntity")
+@Table(name = "band", schema = "jam")
+public class BandEntity {
+    @Id
+    @Column(name = "band_id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "jam.band_id_seq")
+    @SequenceGenerator(schema = "jam", name = "jam.band_id_seq", sequenceName = "jam.band_id_seq", initialValue = 1, allocationSize = 1)
+    private int id;
+
+    @Column(name = "band_name")
+    private String bandName;
+
+    @Column(name = "musical_genre")
+    private String musicalGenre;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.PERSIST})
+    @JoinColumn(name = "musician_id")
+    private MusicianEntity musicianEntity;
+
+
+    // --- GETTERS & SETTERS --- //
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBandName() {
+        return bandName;
+    }
+
+    public void setBandName(String bandName) {
+        this.bandName = bandName;
+    }
+
+    public String getMusicalGenre() {
+        return musicalGenre;
+    }
+
+    public void setMusicalGenre(String musicalGenre) {
+        this.musicalGenre = musicalGenre;
+    }
+
+    public MusicianEntity getMusicianEntity() {
+        return musicianEntity;
+    }
+
+    public void setMusicianEntity(MusicianEntity musicianEntity) {
+        this.musicianEntity = musicianEntity;
+    }
+
+
+    // --- BUILDER --- //
+
+    public static final class Builder {
+        private int id;
+        private String bandName;
+        private String musicalGenre;
+        private MusicianEntity musicianEntity;
+
+        private Builder() {
+        }
+
+        public static Builder aBandEntity() {
+            return new Builder();
+        }
+
+        public Builder withId(int id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder withBandName(String bandName) {
+            this.bandName = bandName;
+            return this;
+        }
+
+        public Builder withMusicalGenre(String musicalGenre) {
+            this.musicalGenre = musicalGenre;
+            return this;
+        }
+
+        public Builder withMusicianEntity(MusicianEntity musicianEntity) {
+            this.musicianEntity = musicianEntity;
+            return this;
+        }
+
+        public Builder but() {
+            return aBandEntity().withId(id).withBandName(bandName).withMusicalGenre(musicalGenre).withMusicianEntity(musicianEntity);
+        }
+
+        public BandEntity build() {
+            BandEntity bandEntity = new BandEntity();
+            bandEntity.setId(id);
+            bandEntity.setBandName(bandName);
+            bandEntity.setMusicalGenre(musicalGenre);
+            bandEntity.setMusicianEntity(musicianEntity);
+            return bandEntity;
+        }
+    }
+
+
+    // --- EQUALS --- //
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (o == null || getClass() != o.getClass()) return false;
+
+        BandEntity that = (BandEntity) o;
+
+        return new EqualsBuilder()
+                .append(id, that.id)
+                .append(bandName, that.bandName)
+                .append(musicalGenre, that.musicalGenre)
+                .append(musicianEntity, that.musicianEntity)
+                .isEquals();
+    }
+
+
+    // --- HASHCODE --- //
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .append(id)
+                .append(bandName)
+                .append(musicalGenre)
+                .append(musicianEntity)
+                .toHashCode();
+    }
+
+
+    // --- TO STRING --- //
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .append("id", id)
+                .append("bandName", bandName)
+                .append("musicalGenre", musicalGenre)
+                .append("musicianEntity", musicianEntity)
+                .toString();
+    }
+}
---
+++ LetsJam/src/main/java/com/letsjam/entities/MusicianEntity.java
@@ -0,0 +1,270 @@
+package com.letsjam.entities;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity(name = "MusicianEntity")
+@Table(name = "musician", schema ="jam")
+public class MusicianEntity implements Serializable {
+
+    private static final long serialVersionUID = -8260206893010106502L;
+
+    @Id
+    @Column(name = "musician_id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "jam.musician_id_seq")
+    @SequenceGenerator(schema = "jam", name = "jam.musician_id_seq", sequenceName = "jam.musician_id_seq", initialValue = 1, allocationSize = 1)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "age")
+    private int age;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "musical_instrument")
+    private String musicalInstrument;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @OneToOne(fetch = FetchType.EAGER, cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.PERSIST})
+    @JoinColumn(name = "username")
+    private LoginEntity loginEntity;
+
+    @LazyCollection(LazyCollectionOption.FALSE)
+    @OneToMany(mappedBy = "musicianEntity", cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.PERSIST})
+    private List<BandEntity> bandEntities;
+
+
+    // --- GETTERS & SETTERS --- //
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getMusicalInstrument() {
+        return musicalInstrument;
+    }
+
+    public void setMusicalInstrument(String musicalInstrument) {
+        this.musicalInstrument = musicalInstrument;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LoginEntity getLoginEntity() {
+        return loginEntity;
+    }
+
+    public void setLoginEntity(LoginEntity loginEntity) {
+        this.loginEntity = loginEntity;
+    }
+
+    public List<BandEntity> getBandEntities() {
+        return bandEntities;
+    }
+
+    public void setBandEntities(List<BandEntity> bandEntities) {
+        this.bandEntities = bandEntities;
+    }
+
+
+    // --- BUILDER --- //
+
+    public static final class Builder {
+        private int id;
+        private String name;
+        private String surname;
+        private int age;
+        private String city;
+        private String musicalInstrument;
+        private String email;
+        private LoginEntity loginEntity;
+        private List<BandEntity> bandEntities;
+
+        private Builder() {
+        }
+
+        public static Builder aMusicianEntity() {
+            return new Builder();
+        }
+
+        public Builder withId(int id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder withName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder withSurname(String surname) {
+            this.surname = surname;
+            return this;
+        }
+
+        public Builder withAge(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public Builder withCity(String city) {
+            this.city = city;
+            return this;
+        }
+
+        public Builder withMusicalInstrument(String musicalInstrument) {
+            this.musicalInstrument = musicalInstrument;
+            return this;
+        }
+
+        public Builder withEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public Builder withLoginEntity(LoginEntity loginEntity) {
+            this.loginEntity = loginEntity;
+            return this;
+        }
+
+        public Builder withBandEntities(List<BandEntity> bandEntities) {
+            this.bandEntities = bandEntities;
+            return this;
+        }
+
+        public Builder but() {
+            return aMusicianEntity().withId(id).withName(name).withSurname(surname).withAge(age).withCity(city).withMusicalInstrument(musicalInstrument).withEmail(email).withLoginEntity(loginEntity).withBandEntities(bandEntities);
+        }
+
+        public MusicianEntity build() {
+            MusicianEntity musicianEntity = new MusicianEntity();
+            musicianEntity.setId(id);
+            musicianEntity.setName(name);
+            musicianEntity.setSurname(surname);
+            musicianEntity.setAge(age);
+            musicianEntity.setCity(city);
+            musicianEntity.setMusicalInstrument(musicalInstrument);
+            musicianEntity.setEmail(email);
+            musicianEntity.setLoginEntity(loginEntity);
+            musicianEntity.setBandEntities(bandEntities);
+            return musicianEntity;
+        }
+    }
+
+
+    // --- EQUALS --- //
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (o == null || getClass() != o.getClass()) return false;
+
+        MusicianEntity that = (MusicianEntity) o;
+
+        return new EqualsBuilder()
+                .append(id, that.id)
+                .append(age, that.age)
+                .append(name, that.name)
+                .append(surname, that.surname)
+                .append(city, that.city)
+                .append(musicalInstrument, that.musicalInstrument)
+                .append(email, that.email)
+                .append(loginEntity, that.loginEntity)
+                .append(bandEntities, that.bandEntities)
+                .isEquals();
+    }
+
+
+    // --- HASHCODE --- //
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .append(id)
+                .append(name)
+                .append(surname)
+                .append(age)
+                .append(city)
+                .append(musicalInstrument)
+                .append(email)
+                .append(loginEntity)
+                .append(bandEntities)
+                .toHashCode();
+    }
+
+
+    // --- TO STRING --- //
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .append("id", id)
+                .append("name", name)
+                .append("surname", surname)
+                .append("age", age)
+                .append("city", city)
+                .append("musicalInstrument", musicalInstrument)
+                .append("email", email)
+                .append("loginEntity", loginEntity)
+                .append("bandEntities", bandEntities)
+                .toString();
+    }
+}
